42d53228c4e059e2f3a1ec4126bed03902362c42
DevNeoLee <devneolee@gmail.com>
2022-05-06 10:40:13 -0700

    Change apiAddress to production rdy

--- utils/apiAddress.js
+++ utils/apiAddress.js
@@ -1,9 +1,11 @@
 export let apiAddress;
     
 if (process.env.NODE_ENV === 'productions') {
-    apiAddress = 'https://pcb365-4-8zxrritcr-justinlee215.vercel.app'
+    apiAddress = 'https://pcb365-4.vercel.app'
 } else {
-    apiAddress = 'http://localhost:3000'
+    // apiAddress = 'http://127.0.0.1'
+    // apiAddress = 'http://localhost:3000'
+    apiAddress = 'https://pcb365-4.vercel.app'
 }
 
 
